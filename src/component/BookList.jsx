4ac284f8e8a4d04602fbb28b0f8bd1f908199811
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-04-21 23:33:55 +0100

    search Query

--- src/component/BookList.jsx
+++ src/component/BookList.jsx
@@ -1,10 +1,41 @@
 import SingleBook from "./SingleBook";
+import { Col, Container, Row, Form } from "react-bootstrap";
+import React from "react";
 
-const BookList = ({ books }) => (
-  <>
-    {books.map((book) => (
-      <SingleBook singleBook={book} />
-    ))}
-  </>
-);
+class BookList extends React.Component {
+  state = {
+    searchQuery: "",
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Search</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Search here"
+                value={this.state.searchQuery}
+                onChange={(e) => this.setState({ searchQuery: e.target.value })}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          {this.props.books
+            .filter((book) =>
+              book.title.toLowerCase().includes(this.state.searchQuery)
+            )
+            .map((book) => (
+              <Col xs={3}>
+                <SingleBook singleBook={book} />
+              </Col>
+            ))}
+        </Row>
+      </Container>
+    );
+  }
+}
 export default BookList;
--- src/component/SingleBook.jsx
+++ src/component/SingleBook.jsx
@@ -1,21 +1,11 @@
-import { Col, Card } from "react-bootstrap";
-
-/*
-props {
-    singleBook:book
-}
-*/
+import { Card } from "react-bootstrap";
 
 const SingleBook = ({ singleBook }) => (
-  <div className="col-md-2">
-    <Col md={2}>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={singleBook.img} />
-        <Card.Body>
-          <Card.Title>{singleBook.title}</Card.Title>
-        </Card.Body>
-      </Card>
-    </Col>
-  </div>
+  <Card>
+    <Card.Img variant="top" src={singleBook.img} />
+    <Card.Body>
+      <Card.Title>{singleBook.title}</Card.Title>
+    </Card.Body>
+  </Card>
 );
 export default SingleBook;
